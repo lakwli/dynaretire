a7e975d1ba0b1a4ddd2c5ca6a23ac3aa0c6fbf0a
lakwli <kwailing.freedom@gmail.com>
2025-03-12 12:35:42 +0000

    robots

--- app.py
+++ app.py
@@ -111,5 +111,9 @@ def privacy():
 def sitemap():
     return send_from_directory('static', 'sitemap.xml', mimetype='application/xml')
 
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('static', 'robots.txt')
+
 if __name__ == '__main__':
     app.run(debug=True)
