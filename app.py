9c7a42ad369a51784c14b126cdf02a961067e87f
lakwli <kwailing.freedom@gmail.com>
2025-03-15 12:47:41 +0000

    basic showcase

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def strategic():
 
 @app.route('/showcase.html')
 def showcase():
-    return render_template('marketing/showcase.html')
+    return render_template('marketing/showcase2.html')
 
 @app.route('/privacy.html')
 def privacy():
