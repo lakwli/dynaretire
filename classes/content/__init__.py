79f225802faa10deabae211576c6abdf64e268d2
lakwli <kwailing.freedom@gmail.com>
2025-03-17 02:59:39 +0000

    CMS: support articles in html based

--- classes/content/__init__.py
+++ classes/content/__init__.py
@@ -7,14 +7,14 @@ from .models.blog import Blog
 from .models.article import Article
 from .repositories.blog_repo import BlogRepository
 from .repositories.blog_html import BlogHtmlRepository
-from .repositories.article_markdown import ArticleMarkdownRepository
+from .repositories.article_html import ArticleHtmlRepository
 from .manager import ContentManager
 
 # Create singleton instances of repositories
 blog_repository = BlogHtmlRepository("content/blog")
-article_repository = ArticleMarkdownRepository("content/articles")
+article_repository = ArticleHtmlRepository("content/articles")
 
-# Create a singleton instance of the manager with HTML blog and markdown article repositories
+# Create a singleton instance of the manager with HTML repositories
 blog_manager = ContentManager(blog_repository, article_repository)
 
 __all__ = [
